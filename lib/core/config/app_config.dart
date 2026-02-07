63053604f48d7cbbcee537f155deb38b0deb39f8
fedorstepanovv <fedorstepanov0903@gmail.com>
2026-02-07 18:42:22 +0200

    add dio to sl

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,4 +1,6 @@
 class AppConfig {
   AppConfig._();
   static final baseUrl = "https://dummy.restapiexample.com/api/v1";
+  static const sendTimeout = Duration(seconds: 12);
+  static const receiveTimeout = Duration(seconds: 12);
 }
--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,21 @@
+import 'package:carcutter_test/core/config/app_config.dart';
+import 'package:carcutter_test/core/network/api_client.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 
-void setupServiceLocator() {}
+void setupServiceLocator() {
+  sl.registerLazySingleton<Dio>(() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: AppConfig.baseUrl,
+        sendTimeout: AppConfig.sendTimeout,
+        receiveTimeout: AppConfig.receiveTimeout,
+      ),
+    );
+    dio.interceptors.add(LogInterceptor());
+    return dio;
+  });
+  sl.registerLazySingleton<ApiClient>(() => ApiClient(sl<Dio>()));
+}
